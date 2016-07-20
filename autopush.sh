34dce2dce821c262cdf717af088c21f1d0caacf6
Seth Silesky <seth.silesky@gmail.com>
2016-07-20 11:18:44 -0500

    autopush

--- autopush.sh
+++ autopush.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
+# don't need semicolons bc the newline is also a command seperator
 source '/Users/ssilesky/.bash_profile'
 cd '/Users/ssilesky/projects/practicetimer' && git push
-cd '/Users/ssilesky' && /usr/local/Cellar/git/2.8.4/bin/git add .
-/usr/local/Cellar/git/2.8.4/bin/git commit -m 'autopush' && git push
+cd '/Users/ssilesky' && /usr/local/Cellar/git/2.8.4/bin/git add . && /usr/local/Cellar/git/2.8.4/bin/git commit -m 'autopush' && git push
 cd '/Users/ssilesky/Library/Application Support/Sublime Text 3/Packages/User/' && gist -u 8d3b87b2aac5cc0b3805738540320dcd *.sublime-settings
