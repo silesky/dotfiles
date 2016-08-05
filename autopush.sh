eeaeac974324bdbce2133b9e26f8071b83b34295
Seth Silesky <seth.silesky@gmail.com>
2016-08-05 15:17:12 -0500

    autopush

--- autopush.sh
+++ autopush.sh
@@ -5,4 +5,4 @@
 source '/Users/ssilesky/.bash_profile'
 cd '/Users/ssilesky/projects/practicetimer' && git push
 cd '/Users/ssilesky' && /usr/local/Cellar/git/2.8.4/bin/git add . && /usr/local/Cellar/git/2.8.4/bin/git commit -m 'autopush' && git push
-cd '/Users/ssilesky/Library/Application Support/Sublime Text 3/Packages/User/' && find "`$(cd ..; pwd)`" -name ‘*.sublime-settings’ -print0 | xargs -0 gist -o -u 8d3b87b2aac5cc0b3805738540320dcd
+cd '/Users/ssilesky/Library/Application Support/Sublime Text 3/Packages/User/' && find "$(cd .; pwd)" -name ‘*.sublime-settings’ -print0 | xargs -0 gist -o -u 8d3b87b2aac5cc0b3805738540320dcd
