2e45b6e7efbf37393026c05b804372f4afc9d6ee
Seth Silesky <seth.silesky@gmail.com>
2016-08-22 13:03:41 -0500

    update gitignore... working

---
+++ scripts/google.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Searching for: $@"
+for term in $@ ; do
+    search="$search%20$term"
+done
+open "http://www.google.com/search?q=$search"
---
+++ scripts/haltall.sh
@@ -0,0 +1,1 @@
+vagrant global-status | grep virtualbox | cut -c 1-9 | while read line; do echo $line; vagrant halt $line; done;
