75500b7048ef173b65c920270a2f6747212e0a95
Seth Silesky <seth.silesky@gmail.com>
2016-09-02 16:28:34 -0500

    autopush

--- scripts/autopush.sh
+++ scripts/autopush.sh
@@ -3,4 +3,4 @@
 # don't need semicolons bc the newline is also a command seperator
 source '/Users/ssilesky/.bash_profile'
 cd '/Users/ssilesky' && /usr/local/Cellar/git/2.8.4/bin/git add . && /usr/local/Cellar/git/2.8.4/bin/git commit -m 'autopush' && git push
-cd '/Users/ssilesky/Library/Application Support/Sublime Text 3/Packages/User/' && find "$(cd ..; pwd)" -name "*.sublime-settings" -print0 | xargs -0 gist -u 8d3b87b2aac5cc0b3805738540320dcd
+cd '/Users/ssilesky/Library/Application Support/Sublime Text 3/Packages/User/' && find "$(cd ..; pwd)" -iname "*.sublime-settings" -or -path "*.sublime-keymap" -print0 | xargs -0 gist -u 8d3b87b2aac5cc0b3805738540320dcd
