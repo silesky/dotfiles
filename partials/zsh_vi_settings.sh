f89a7fee204c9f000be35ea0970a4bc80d83c337
silesky <seth.silesky@gmail.com>
2017-03-31 15:00:00 -0500

    cronpush

--- partials/zsh_vi_settings.sh
+++ partials/zsh_vi_settings.sh
@@ -4,12 +4,12 @@ precmd() { RPROMPT="" }
 bindkey '^r' history-incremental-search-backward
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
-function zle-line-init zle-keymap-select {
-   VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
-   RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
-   zle reset-prompt
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
+#function zle-line-init zle-keymap-select {
+#   VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+#   RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+#   zle reset-prompt
+#}
+#zle -N zle-line-init
+#zle -N zle-keymap-select
 export KEYTIMEOUT=1
 
