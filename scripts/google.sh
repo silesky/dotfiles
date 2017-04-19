55354118ac77da0b9c4ba0c60b6a84b946bf90fc
silesky <seth.silesky@gmail.com>
2017-04-19 10:00:01 -0500

    cronpush

--- scripts/google.sh
+++ scripts/google.sh
@@ -3,4 +3,4 @@ echo "Searching for: $@"
 for term in $@ ; do
     search="$search%20$term"
 done
-open "http://www.google.com/search?q=$search"
+open "http://www.google.com/search?q=$search" --args --profile-directory=Default
