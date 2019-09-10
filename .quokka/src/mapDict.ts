b930bed1920ed5964f1711d48057f1f6ed03d88e
Seth Silesky <seth.silesky@spreetail.com>
2019-09-10 13:00:00 -0500

    autopush

---
+++ .quokka/src/mapDict.ts
@@ -0,0 +1,11 @@
+type MapDict = <Key extends string, V, V2>(
+  obj: Record<Key, V>,
+  exp: (arg: V) => V2,
+) => Record<Key, V2>;
+
+const mapDict: MapDict = (obj, fn) => {
+  return Object.assign({}, ...Object.keys(obj).map(k => ({ [k]: fn(obj[k]) })));
+};
+
+const f = mapDict({ name: 123, 1: '123' }, x => x.split(''));
+console.log(f);
