55c3f0d4389fcff7bd686e0664251ec14923fcf8
Seth Silesky <seth.silesky@spreetail.com>
2019-09-10 20:00:00 -0500

    autopush

--- .quokka/src/mapDict.ts
+++ .quokka/src/mapDict.ts
@@ -7,5 +7,17 @@ const mapDict: MapDict = (obj, fn) => {
   return Object.assign({}, ...Object.keys(obj).map(k => ({ [k]: fn(obj[k]) })));
 };
 
-const f = mapDict({ name: 123, 1: '123' }, x => x.split(''));
-console.log(f);
+// const f = mapDict({ name: 123, 1: '123' }, x => x.split(''));
+// console.log(f);
+
+type LinkedList<T = any> = {
+  val: T;
+  next?: LinkedList<T>;
+};
+const v = {
+  val: 'fo,
+};
+const o: LinkedList<string> = {
+  val: 'foo',
+  next: v,
+};
