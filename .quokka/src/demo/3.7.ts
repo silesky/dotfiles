cbe8c668f8ac824c0b8f9e80b334b73fd0f96735
Seth Silesky <seth.silesky@spreetail.com>
2020-01-08 12:06:25 -0600

    yep

--- .quokka/src/demo/3.7.ts
+++ .quokka/src/demo/3.7.ts
@@ -14,18 +14,18 @@ const res = user?.address?.lineTwo;
 console.log(res);
 
 /***************************************************************** */
-// null(ish) coalescing - https://github.com/tc39/proposal-nullish-coalescing
-let firstResult: string | null | undefined
-const bar = firstResult ?? 'hello'
-console.log(bar) // string
+{
+  // null(ish) coalescing - https://github.com/tc39/proposal-nullish-coalescing
+  let firstResult: string | null | undefined;
+  const bar = firstResult ?? 'hello';
+  console.log(bar); // string
 
-
-
-let firstResult1: boolean | null | undefined
-const bar1 = firstResult1 ?? true
-console.log(bar1) // boolean
-
+  let firstResult1: boolean | null | undefined;
+  const bar1 = firstResult1 ?? true;
+  console.log(bar1); // boolean
+}
 /***************************************************************** */
+{
 // traditional typeguard
 const isString = (input: unknown): input is string => {
   return typeof input === 'string';
@@ -37,27 +37,26 @@ const doSomethingWithTraditionalGuard = (input: string | number) => {
   }
 };
 
-// must be declared with the function keyword
-// can throw errors
-function assertString(input: unknown): asserts input is string {
-  if (typeof input === 'string') return;
-  throw new Error('Input must be a string!');
-}
-
-const doSomething = (input: unknown) => {
-  assertString(input);
-  console.log(input);
-};
-
-// more stuff
-function assertUndefined<T>(v: T): asserts v is NonNullable<T> {
-  if (v === undefined || v === null) {
-    throw new Error('undefined!')
+  // must be declared with the function keyword
+  // can throw errors
+  function assertString(input: unknown): asserts input is string {
+    if (typeof input === 'string') return;
+    throw new Error('Input must be a string!');
   }
+
+  const doSomething = (input: unknown) => {
+    assertString(input);
+    console.log(input);
+  };
+
+  // more stuff
+  function assertUndefined<T>(v: T): asserts v is NonNullable<T> {
+    if (v === undefined || v === null) {
+      throw new Error('undefined!');
+    }
+  }
+  // better
+  let o: string | number | undefined;
+  assertUndefined(o);
+  console.log(o);
 }
-
-// better
-let o: string | number | undefined
-assertUndefined(o)
-console.log(o)
-
