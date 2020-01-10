498685deabba780b06ec76e6c37271f1c2cc2dd2
Seth Silesky <seth.silesky@spreetail.com>
2020-01-10 12:00:00 -0600

    autopush

--- .quokka/src/mapDict.ts
+++ .quokka/src/mapDict.ts
@@ -1,16 +1,11 @@
-import { compose } from 'rambda';
-
-compose(
-  (x: string): string => `${x}foo`,
-  (x: number): string => `${x}`,
-  (x: number): number => x * 2,
-);
-
-type MapDict = <Key extends string, V, V2>(
+const mapDict = <Key extends string, V, V2>(
   obj: Record<Key, V>,
-  exp: (arg: V) => V2,
-) => Record<Key, V2>;
-
-const mapDict: MapDict = (obj, fn) => {
-  return Object.assign({}, ...Object.keys(obj).map(k => ({ [k]: fn(obj[k]) })));
+  fn: (arg: V) => V2
+): Record<Key, V2> => {
+  return Object.assign(
+    {},
+    ...Object.keys(obj).map(k => ({
+      [k]: fn(obj[k])
+    }))
+  );
 };
