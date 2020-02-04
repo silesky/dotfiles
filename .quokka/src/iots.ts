a9698669943f996f5855e87372e0dcf2103c4538
Seth Silesky <seth.silesky@gmail.com>
2020-02-04 12:00:00 -0600

    autopush

--- .quokka/src/iots.ts
+++ .quokka/src/iots.ts
@@ -1,4 +1,20 @@
 import * as t from 'io-ts';
+import { PathReporter } from 'io-ts/lib/PathReporter';
+const { report } = PathReporter;
 
+const fetchProduct = (): Promise<Product> => {
+  return Promise.reject('Error');
+};
 
-const t =
+// Product is now a "codec" -- a runtime representation of the static type "Product"
+const Product = t.type({
+  id: t.number,
+  name: t.string,
+  category: t.array(t.string),
+});
+
+type Product = typeof Product;
+
+// left (fail) -> right (success)
+const result = Product.decode({ id: 123 });
+console.log(result);
