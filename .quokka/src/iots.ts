dd0b178f0758815e353e85aa30a6572a71eaff17
Seth Silesky <seth.silesky@gmail.com>
2020-02-04 13:00:15 -0600

    autopush

--- .quokka/src/iots.ts
+++ .quokka/src/iots.ts
@@ -1,20 +1,45 @@
 import * as t from 'io-ts';
 import { PathReporter } from 'io-ts/lib/PathReporter';
-const { report } = PathReporter;
+import * as tPromise from 'io-ts-promise';
 
-const fetchProduct = (): Promise<Product> => {
-  return Promise.reject('Error');
+const fetchProduct = () => {
+  return Promise.resolve({
+    id: '123',
+    name: '15mg cbd pen',
+    category: ['vapes'],
+    location: {}, // error
+  });
 };
 
 // Product is now a "codec" -- a runtime representation of the static type "Product"
 const Product = t.type({
-  id: t.number,
+  id: t.string,
   name: t.string,
   category: t.array(t.string),
+  location: t.type({
+    warehouses: t.array(t.string),
+  }),
 });
 
-type Product = typeof Product;
+// this special t.typeOf operator gives us the vanilla
+type Product = t.TypeOf<typeof Product>;
 
-// left (fail) -> right (success)
-const result = Product.decode({ id: 123 });
-console.log(result);
+const getProduct = async (): Promise<Product | void> => {
+  try {
+    const product = await fetchProduct().then(tPromise.decode(Product));
+    return product;
+  } catch (err) {
+    if (tPromise.isDecodeError(err)) {
+      console.log('error due to decoding issue');
+    } else {
+      console.log('error due to network issue');
+    }
+    return;
+  }
+};
+
+const main = () => {
+  getProduct();
+};
+
+main();
