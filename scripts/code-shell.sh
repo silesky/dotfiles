d25cfe4cb4de2e09bfe38b33228199652a0744c2
Seth Silesky <seth.silesky@gmail.com>
2020-02-11 14:03:59 -0600

    autopush

--- scripts/code-shell.sh
+++ scripts/code-shell.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-SESSION="vscode`pwd | md5`"
-tmux attach-session -d -t $SESSION || tmux new-session -s $SESSION
-
+if [ "$#" -gt 1 ]; then
+    /bin/sh "$@"
+else
+    #
+    SESSION="vscode`pwd | md5`"
+    tmux attach -d -t $SESSION || tmux new-session -s $SESSION
+fi
