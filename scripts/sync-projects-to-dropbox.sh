c4301c943971a7331dfce31ece85ae476d923335
Seth Silesky <seth.silesky@gmail.com>
2020-02-22 17:00:00 -0600

    autopush

--- scripts/sync-projects-to-dropbox.sh
+++ scripts/sync-projects-to-dropbox.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 CODE_ROOT=~/Dropbox/_code/
-PROJECT_ROOT=~/projects/lm-core
+PROJECT_ROOT=~/projects/
 EXCLUDE_PATH=~/scripts/sync-projects-to-dropbox.exclude.txt
+rsync -avzh --filter=':- .gitignore' --exclude-from "$EXCLUDE_PATH" $PROJECT_ROOT $CODE_ROOT
 
-rsync -avzh --exclude-from "$EXCLUDE_PATH" $PROJECT_ROOT $CODE_ROOT
+# EDITOR=vim crontab -e
+## sync every 15 minutes
+# 0/15 * * * * sh ~/scripts/sync-projects-to-dropbox.sh
