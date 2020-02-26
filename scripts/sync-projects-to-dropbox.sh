849a9b11e241d671f74869b6a0664e83bd5ea180
Seth Silesky <seth.silesky@gmail.com>
2020-02-25 20:00:00 -0600

    autopush

--- scripts/sync-projects-to-dropbox.sh
+++ scripts/sync-projects-to-dropbox.sh
@@ -2,7 +2,7 @@
 CODE_ROOT=~/Dropbox/_code/
 PROJECT_ROOT=~/projects/
 EXCLUDE_PATH=~/scripts/sync-projects-to-dropbox.exclude.txt
-rsync -avzh --filter=':- .gitignore' --exclude-from "$EXCLUDE_PATH" $PROJECT_ROOT $CODE_ROOT
+rsync -avzh --exclude-from "$EXCLUDE_PATH" $PROJECT_ROOT $CODE_ROOT
 
 # EDITOR=vim crontab -e
 ## sync every 15 minutes
