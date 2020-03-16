85f613d39d446c5bc281747da0c97998740f8989
Seth Silesky <seth.silesky@gmail.com>
2020-03-15 19:00:00 -0500

    autopush

--- .quokka/src/ramda.ts
+++ .quokka/src/ramda.ts
@@ -1,27 +1,60 @@
-// Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+const range = num => [...Array(num)].map((_, idx) => idx + 1);
+var spiralOrder = function(matrix: number[][]): number[] {
+  const lastRowIdx = matrix.length - 1;
+  const lastColIdx = matrix[0].length - 1;
+  const totalLength = matrix.reduce((p, n) => p.concat(n)).length;
+  const arr: number[] = [];
+  let colIdx = 0;
+  let rowIdx = 0;
+  while (arr.length < totalLength) {
+    while (true) {
+      arr.push(matrix[rowIdx][colIdx]);
+      if (colIdx === lastColIdx) {
+        break;
+      } else {
+        colIdx++;
+      }
+    }
 
-// Example 1:
+    while (true) {
+      console.log(rowIdx);
+      arr.push(matrix[rowIdx][colIdx]);
+      if (rowIdx !== lastRowIdx) {
+        rowIdx++;
+      } else {
+        break;
+      }
+    }
 
-// Input:
-// [
-//  [ 1, 2, 3 ],
-//  [ 4, 5, 6 ],
-//  [ 7, 8, 9 ]
-// ]
-// Output: [1,2,3,6,9,8,7,4,5]
-// Example 2:
+    while (true) {
+      console.log(rowIdx);
+      arr.push(matrix[rowIdx][colIdx]);
+      if (rowIdx !== lastRowIdx) {
+        rowIdx++;
+      } else {
+        break;
+      }
+    }
 
-// Input:
-// [
-//   [1, 2, 3, 4],
-//   [5, 6, 7, 8],
-//   [9,10,11,12]
-// ]
-// Output: [1,2,3,4,8,12,11,10,9,5,6,7]
-// Accepted
+    while (true) {
+      arr.push(matrix[rowIdx][colIdx]);
+      if (colIdx !== colIdx) {
+        colIdx--;
+        console.log(colIdx);
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+};
 
-/**
- * @param {number[][]} matrix
- * @return {number[]}
- */
-var spiralOrder = function(matrix) {};
+const result = spiralOrder([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+// [1,2,3,6,9,8,7,4,1,2,5]
+
+console.log(result);
+/*?*/
