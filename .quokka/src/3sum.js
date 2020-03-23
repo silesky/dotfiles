67d9a7bf16819d1007ea6b39bd61284349e0d3e2
Seth Silesky <seth.silesky@gmail.com>
2020-03-22 21:00:00 -0500

    autopush

--- .quokka/src/3sum.js
+++ .quokka/src/3sum.js
@@ -1,15 +1,24 @@
-const threeSum = (arr, num) => {
-  const m = {};
-  for (el of arr) {
-
-    const compliment = num - el
-    if (compliment in m) {
-      return [m[compliment], el]
+const twoSum = (arr, sum) => {
+  const m = {}; // key = compliment, value = number
+  for (let el of arr) {
+    if (el in m) {
+      return [m[el], el];
     }
-    m[compliment] = el
+    const compliment = sum - el;
+    m[compliment] = el;
+  }
+  return;
+};
+const threeSum = (arr, sum) => {
+  const pastValues = [];
+  for (let el of arr) {
+    const compliment = sum - el;
+    const twoSums = twoSum(pastValues, compliment);
+    if (twoSums) return [...twoSums, el];
+    pastValues.push(el);
   }
 };
-const arr = [1, 2, 5, 6, 7, 8, 9, 22, 13, 15, 16, 12];
+const arr = [0, 1, 2, 5, 6, 7, 3, 8, 9, 22, 13, 15, 16, 12];
 
 const res = threeSum(arr, 10);
-console.log(JSON.stringify(res))
+console.log(JSON.stringify(res));
