62aa047610dc0fc1275e2b18590ce3410f68a617
Seth Silesky <seth.silesky@gmail.com>
2020-03-25 22:00:07 -0500

    autopush

--- .quokka/assign.ts
+++ .quokka/assign.ts
@@ -3,7 +3,9 @@ import { Dictionary } from 'lodash';
 type GetUnion<T extends any[]> = T[number];
 const assign = <U, T extends {[key: string]: any}>(x: U, ...xs: T[]) => {
   xs.forEach(eachX => {
-    for (let eachKey of eachX) {
+    for (let eachKey in eachX) {
+      // @t
+      x[eachKey] = eachX[eachKey]
 
     }
   })
