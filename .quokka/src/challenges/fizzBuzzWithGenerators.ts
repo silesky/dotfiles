ddc9c3139f52ade5d3e3bf1ddf86bd91b4b1c69b
Seth Silesky <seth.silesky@gmail.com>
2020-04-01 15:00:00 -0500

    autopush

---
+++ .quokka/src/challenges/fizzBuzzWithGenerators.ts
@@ -0,0 +1,17 @@
+const fizzBuzz = () => {
+  const [divBy3, divBy5] = [3, 5].map(n => {
+    return (arg: number) => arg % n === 0;
+  });
+  return [...Array(100).keys()].map(function*(_, idx) {
+    const num = idx + 1;
+    const fizz = divBy3(num);
+    const buzz = divBy5(num);
+    if (fizz) yield 'fizz';
+    if (buzz) yield 'buzz';
+    if (!fizz && !buzz) yield num;
+  });
+};
+
+const res = fizzBuzz()
+  .map((gen) => [...gen].join(''))
+console.log(res)
