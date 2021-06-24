c9868f70940653fccecf7f8b1db748bdd32f7b56
Seth Silesky <seth.silesky@gmail.com>
2021-06-24 13:15:00 -0500

    autopush

--- .quokka/src/deep-map.ts
+++ .quokka/src/deep-map.ts
@@ -1,7 +1,5 @@
-import {default as deepMap} from 'deep-map';
-​
+import { default as deepMap } from 'deep-map';
 type Compute<T> = T extends Function ? T : {} & { [K in keyof T]: T[K] };
-​
 export type DeepExclude<T, U> = Compute<
   T extends (...args: any[]) => any
     ? T
@@ -11,31 +9,23 @@ export type DeepExclude<T, U> = Compute<
     ? DeepExcludeFromObject<T, U>
     : T
 >;
-​
-export type DeepExcludeFromArray<T, U> = Array<DeepExclude<Exclude<T, U>, U>>
-​
+export type DeepExcludeFromArray<T, U> = Array<DeepExclude<Exclude<T, U>, U>>;
 export type DeepExcludeFromObject<T, U> = {
   [P in keyof T]: DeepExclude<Exclude<T[P], U>, U>;
 };
-​
 type MyType = {
   a?: {
     b?: string | undefined | null;
     c?: string | undefined | null;
-  },
+  };
   d?: string | undefined | null;
 };
-​
 const obj: MyType = {
   a: {
     b: 'hello',
     c: null,
   },
   d: null,
-}
+};
 
-
-
-const resultWithoutNull: DeepExclude<MyType, null> = deepMap('abc', 'avc')
-type f = typeof deepMap
-console.log(resultWithoutNull)
+const resultWithoutNull: DeepExclude<MyType, null> = deepMap('abc', 'avc');
